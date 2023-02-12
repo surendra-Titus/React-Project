d334090cee7e38af6437b69c8516d226220b548d
surendra-Titus <nagasurendra66@gmail.com>
2023-02-12 23:40:32 +0530

    Implemented Initial routing.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const AppLayout = () => {
   return (
     <>
@@ -14,5 +15,16 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/About",
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
