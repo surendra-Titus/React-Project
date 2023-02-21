cc76fda7423599d7bfd9cf9507a1c5c832fa308c
surendra-Titus <nagasurendra66@gmail.com>
2023-02-21 23:26:45 +0530

    Nested Routing.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Cart from "./components/Cart";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Profile from "./components/Profile";
 const AppLayout = () => {
   return (
     <>
@@ -31,6 +32,12 @@ const appRouter = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+        ],
       },
       {
         path: "/contact",
--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Profile from "./Profile";
 const About = () => {
-  return <h1>About us page</h1>;
+  return (
+    <>
+      <h1>About us page</h1>
+      <Profile name="Genius" />
+    </>
+  );
 };
 
 export default About;
